47fb1cec59c60f6d40b74447e5ebe7488b50dc40
Raymond Zeng <rzeng3@gmail.com>
2022-12-08 00:40:42 -0800

    Inputs should be floats, not ints

--- row.py
+++ row.py
@@ -1,4 +1,4 @@
 class row:
-    def __init__(self, selectionClass: int, selectionData: list[int]) -> None:
+    def __init__(self, selectionClass: int, selectionData: list[float]) -> None:
         self.selectionClass = selectionClass
         self.selectionData = selectionData
